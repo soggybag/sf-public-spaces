8d2d7fc9b06904af0a0064ec4846887aa32fd168
Mitchell <soggybag@gmail.com>
2019-08-29 16:47:14 -0700

    added font awesome react

--- src/PageContent/PublicSpaceView/PublicSpaceView.js
+++ src/PageContent/PublicSpaceView/PublicSpaceView.js
@@ -1,18 +1,31 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faTree, faHome, faStar, faToilet } from '@fortawesome/free-solid-svg-icons'
+
 import './PublicSpaceView.css'
 
 function PublicSpaceView({ image, title, link }) {
   return (
     <div className="PublicSpaceView">
       <div className="PublicSpaceView-image-container">
-        <img 
+        <img
           className="PublicSpaceView-image-container--img"
+          alt={`${title} image`}
           src={`${process.env.PUBLIC_URL}${image}`} 
           width="300" 
           height="200" />
       </div>
       <h3>{title}</h3>
-      <a href={link}>Link to project</a>
+      <p>
+        <a href={link}>Link to project</a>
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faCoffee} />
+        <FontAwesomeIcon icon={faHome} />
+        <FontAwesomeIcon icon={faTree} />
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faToilet} />
+      </p>
     </div>
   ) 
 }
